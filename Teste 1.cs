6c46311bb625ab259a495fcaec664013e0ba591b
Matheus Felipe Burigo <67557518+MatheusBurigo@users.noreply.github.com>
2023-03-02 18:48:49 -0300

    Add files via upload

---
+++ Teste 1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio
+{
+    public class Teste_1
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 7, 5, 3, 9, 6, 4, 1 };
+            int sum = 0;
+
+            // Imprime a lista original
+            Console.WriteLine("Original List: ");
+            foreach (var index in numbers)
+            {
+                Console.Write(index + " ");
+            }
+
+            //Valida o 9 e troca por 5
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 9)
+                {
+                    numbers[i] = 5;
+                }
+            }
+            //remove o 4
+            numbers = Array.FindAll(numbers, n => n != 4);
+
+            Console.WriteLine("\nLista modificada: ");
+            foreach (var numero in numbers)
+            {
+                Console.Write(numero + " ");
+            }
+
+            // Calcula e imprime a soma dos valores da nova lista
+            foreach (int index in numbers)
+            {
+                sum += index;
+            }
+            Console.WriteLine($"\nSoma dos valores da nova modificada: {sum}");
+        }
+    }
+}
---
+++ Teste 3.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 2, 7, 11, 15 };
+        var enter = 9;
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (dictionary.ContainsKey(enter - numbers[i]))
+            {
+                var index = dictionary[enter - numbers[i]];
+                Console.WriteLine(
+                    $"Valor de soma da entrada: {enter}\n{enter} é a soma entre {numbers[index]} "
+                    + $"+ {numbers[i]}\n{numbers[index]} "
+                    + $"+ {numbers[i]} são os valores do de entrada.");
+                return;
+            }
+            dictionary[numbers[i]] = i;
+        }
+    }
+}
